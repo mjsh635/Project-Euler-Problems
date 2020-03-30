988ea0c1933f649c55244ee63aeaf174eb6747ce
MSH <mjsh635@gmail.com>
2020-03-30 11:20:27 -0400

    Start of problem 3, largest prime factors

---
+++ Problem3LargestPrimeNumber.py
@@ -0,0 +1,17 @@
+"""The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?"
+"""
+
+def Largest_Prime_Factor(number):
+    """ Return the larges prime factor of inputed number.
+
+    Parameters:
+    number (int): number to be factored
+
+    Return:
+    int: returns the largest prime number
+    """
+    pass
+
+print(Largest_Prime_Factor(13195))
