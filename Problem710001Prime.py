e299938e547a8fc7fa1427268a276806f9b5a2ac
MSH <mjsh635@gmail.com>
2020-04-06 19:49:48 -0400

    Laying framework for Problem 7

---
+++ Problem710001Prime.py
@@ -0,0 +1,19 @@
+"""By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+"""
+
+def target_prime(nth_prime):
+    """
+    Return the nth prime number
+
+    Parameters:
+    nth_prime (int): the nth prime number
+
+    Return:
+    int : the prime number that is nth 
+    """
+
+    pass
+
+print(target_prime(6))
